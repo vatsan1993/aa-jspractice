6bd01082dd8533492449f7e418b9300c10e1dce4
Srivatsan <vatsan1993@gmail.com>
2023-04-14 19:22:34 +0530

    js objects 2

---
+++ intemediate/jsObjects/catBuilder.js
@@ -0,0 +1,14 @@
+function catBuilder(name, color, toys) {
+    // your code here...
+    return {
+        name : name,
+        color : color,
+        toys : toys
+    }
+  }
+
+  console.log(catBuilder("Whiskers", "black", ["scratching-post", "yarn"]));
+  // prints: { name: 'Whiskers', color: 'black', toys: ['scratching-post', 'yarn'] }
+
+  console.log(catBuilder("Nyan", "rainbow", ["poptarts"]));
+  // prints: { name: 'Nyan', color: 'rainbow', toys: [ 'poptarts' ] }
